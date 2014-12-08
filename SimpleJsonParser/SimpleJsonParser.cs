666999e19caeeaecab8169e8d6395f97b454cb84
acple <silentsphere110@gmail.com>
2014-12-08 22:42:40 +0900

    Initial commit

---
+++ SimpleJsonParser/SimpleJsonParser.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SimpleJsonParser
+{
+    public static class JsonParser
+    {
+        public static JsonElement Parse(string json)
+        {
+            return Parse(json, Encoding.UTF8);
+        }
+
+        public static JsonElement Parse(string json, Encoding encoding)
+        {
+            using (var reader = JsonReaderWriterFactory.CreateJsonReader(encoding.GetBytes(json), XmlDictionaryReaderQuotas.Max))
+            {
+                return new JsonElement(XElement.Load(reader));
+            }
+        }
+    }
+
+    public enum JsonType : byte { Int, Double, String, Array, Object, Bool, Null }
+
+    public class JsonElement : IEnumerable<JsonElement>
+    {
+        private dynamic Value; //long, double, string, List<JsonElement>, Dictionary<string, JsonElement>, bool, null
+
+        public string Name { get; private set; }
+
+        public JsonType Type { get; private set; }
+
+        public long Int { get { return (long)GetValueWith(JsonType.Int, JsonType.Double); } }
+
+        public double Double { get { return (double)GetValueWith(JsonType.Double, JsonType.Int); } }
+
+        public string String { get { return GetValueWith(JsonType.String); } }
+
+        public bool Bool { get { return GetValueWith(JsonType.Bool); } }
+
+        public JsonElement this[string key]
+        {
+            get
+            {
+                try { return GetValueWith(JsonType.Object)[key]; }
+                catch (KeyNotFoundException ex)
+                { throw new KeyNotFoundException(string.Format("要素名 \"{0}\" が [{1}] に存在しません", key, this.Name), ex); }
+            }
+        }
+
+        public JsonElement this[int index]
+        {
+            get { return GetValueWith(JsonType.Array)[index]; }
+        }
+
+        public bool Exists(string name)
+        {
+            return GetValueWith(JsonType.Object).ContainsKey(name);
+        }
+
+        public int Count()
+        {
+            return GetValueWith(JsonType.Array, JsonType.Object).Count;
+        }
+
+        public bool TypeIs(params JsonType[] type)
+        {
+            return type.Contains(this.Type);
+        }
+
+        private dynamic GetValueWith(params JsonType[] types)
+        {
+            if (TypeIs(types))
+                return this.Value;
+            throw new InvalidOperationException(string.Format("要素 \"{0}\" は {1} 型ではありません",
+                this.Name, string.Join(" 型、", types.Select(x => "<" + x.ToString() + ">"))));
+        }
+
+        private static IDictionary<string, JsonElement> JsonObject(IEnumerable<XElement> elements)
+        {
+            return elements.ToDictionary(x => GetElementName(x), x => new JsonElement(x));
+        }
+
+        private static IList<JsonElement> JsonArray(IEnumerable<XElement> elements)
+        {
+            return elements.Select(x => new JsonElement(x)).ToList();
+        }
+
+        private static string GetElementName(XElement element)
+        {
+            return element.Attribute("item") != null ? element.Attribute("item").Value : element.Name.LocalName;
+        }
+
+        private void Set(dynamic value, string name, JsonType type)
+        {
+            this.Value = value;
+            this.Name = name;
+            this.Type = type;
+        }
+
+        internal JsonElement(XElement element)
+        {
+            switch (element.Attribute("type").Value)
+            {
+                case "number":
+                    long l;
+                    double d;
+                    if (long.TryParse(element.Value, out l))
+                        Set(l, GetElementName(element), JsonType.Int);
+                    else if (double.TryParse(element.Value, out d))
+                        Set(d, GetElementName(element), JsonType.Double);
+                    else
+                        throw new ArgumentException("numberのパースに失敗しました");
+                    break;
+                case "string":
+                    Set(element.Value, GetElementName(element), JsonType.String);
+                    break;
+                case "array":
+                    Set(JsonArray(element.Elements()), GetElementName(element), JsonType.Array);
+                    break;
+                case "object":
+                    Set(JsonObject(element.Elements()), GetElementName(element), JsonType.Object);
+                    break;
+                case "boolean":
+                    try { Set(bool.Parse(element.Value), GetElementName(element), JsonType.Bool); }
+                    catch (Exception ex) { throw new ArgumentException("booleanのパースに失敗しました", ex); }
+                    break;
+                case "null":
+                    Set(null, GetElementName(element), JsonType.Null);
+                    break;
+                default:
+                    throw new ArgumentException("JsonTypeの判別に失敗しました");
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (this.Type)
+            {
+                case JsonType.Int:
+                case JsonType.Double: return this.Value.ToString();
+                case JsonType.String: return "\"" + ReplaceEscapeChars(this.Value) + "\"";
+                case JsonType.Array: return "[" + string.Join(",", this.Select(x => x.ToString())) + "]";
+                case JsonType.Object: return "{" + string.Join(",", this.Select(x => "\"" + ReplaceEscapeChars(x.Name) + "\":" + x.ToString())) + "}";
+                case JsonType.Bool: return this.Value.ToString().ToLower();
+                case JsonType.Null: return "null";
+                default: return null;
+            }
+        }
+
+        private static string ReplaceEscapeChars(string str)
+        {
+            return str.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n").Replace("\r", "\\r")
+                .Replace("\t", "\\t").Replace("\f", "\\f").Replace("\b", "\\b").Replace("/", "\\/");
+        }
+
+        public IEnumerator<JsonElement> GetEnumerator()
+        {
+            if (this.Type == JsonType.Object)
+            {
+                foreach (KeyValuePair<string, JsonElement> element in this.Value)
+                    yield return element.Value;
+            }
+            else if (this.Type == JsonType.Array)
+            {
+                foreach (JsonElement element in this.Value)
+                    yield return element;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
